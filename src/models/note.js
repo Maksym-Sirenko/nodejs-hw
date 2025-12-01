883bc0a1e044c6a9306cfdbbf04bcd792b52eac5
Maksym-Sirenko <toyotasan@gmail.com>
2025-12-01 17:11:54 +0200

    add routes and controllers

---
+++ src/models/note.js
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+
+const notesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: { type: String, trim: true, required: false, default: "" },
+    tag: {
+      type: String,
+      enum: [
+        "Work",
+        "Personal",
+        "Meeting",
+        "Shopping",
+        "Ideas",
+        "Travel",
+        "Finance",
+        "Health",
+        "Important",
+        "Todo",
+      ],
+      default: "Todo",
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const NoteModel = model("Note", notesSchema, "notes");
