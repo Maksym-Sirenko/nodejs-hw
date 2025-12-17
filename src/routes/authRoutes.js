2002c53eece1efec4d4992a98671a4f68f951b41
Maksym Sirenko <toyotasan@gmail.com>
2025-12-17 11:38:48 +0200

    Start 05-mail-and-img

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,11 @@
 
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, logoutUser, refreshUserSession, registerUser } from '../controllers/authController.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetEmail } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -14,5 +15,6 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
 
 export default router;
