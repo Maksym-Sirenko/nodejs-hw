7464ec756043fffcf9275160ba80621f438639f7
Maksym Sirenko <toyotasan@gmail.com>
2025-12-22 18:05:51 +0200

    fix: allow any image/* mimetype in multer fileFilter

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,14 @@
 
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetEmail, resetPassword } from '../controllers/authController.js';
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+  requestResetEmail,
+  resetPassword,
+} from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -16,7 +23,15 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
-router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
-router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 export default router;
